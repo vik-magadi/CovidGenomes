0ff55c7a1d5d79608201e1f53ee6689f22d9ed2f
Vikrant Magadi <vik.magadi@gmail.com>
2024-10-22 22:50:16 -0400

    unpacking zip

---
+++ ncbi_api_scripts/get_covid_genome.py
@@ -0,0 +1,31 @@
+import requests
+import zipfile
+import io
+import json
+
+taxon = "2697049"
+wuhan_accession = "NC_045512.2"
+
+def get_genome_data(accession):
+    data = {
+        "accessions":[accession],
+        "taxon": taxon,
+        "include_sequence":["GENOME"],
+        "aux_report":["DATASET_REPORT"],
+        "format":"csv",
+    }
+
+    headers= {
+        "accept": "application/zip",
+        "api-key": "6c1c34d9083e10e74a368375c29d9cdb5908",
+        "content-type": "application/json",
+    }
+
+    genomeZip = requests.post("https://api.ncbi.nlm.nih.gov/datasets/v2/virus/genome/download", data=json.dumps(data), headers=headers)
+    z = zipfile.ZipFile(io.BytesIO(genomeZip.content))
+    z.extract("ncbi_dataset/data/genomic.fna", path="./genome_data/" + accession)
+    
+get_genome_data(wuhan_accession)
+
+
+
